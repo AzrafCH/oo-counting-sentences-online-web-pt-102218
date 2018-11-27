63dc873c2cb5bceddb305ae7ae63a7a4d21bd6a1
Azraf Chowdhury <achowdhur@ltu.edu>
2018-11-27 21:19:09 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,7 +1,18 @@
 class String 
 
-def sentence
+def sentence?
   self.end_with?(".")
 end
+
+def question?
+  self.end_with?("?")
+end 
+
+def exclamation?
+  self.end_with?("!")
+end 
+
+def count_sentences
+  self.split.
 
 end 
